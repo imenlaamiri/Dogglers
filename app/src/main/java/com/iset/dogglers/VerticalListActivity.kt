7b0f6216468445df059f11a32e9df5f6a92efaa2
imenlaamiri <imenlaamiri9@gmail.com>
2021-11-20 13:18:49 +0100

    Initial commit

---
+++ app/src/main/java/com/iset/dogglers/VerticalListActivity.kt
@@ -0,0 +1,29 @@
+package com.iset.dogglers
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.iset.dogglers.adapter.DogCardAdapter
+import com.iset.dogglers.const.Layout
+import com.iset.dogglers.databinding.ActivityVerticalListBinding
+
+class VerticalListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityVerticalListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityVerticalListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.verticalRecyclerView.adapter = DogCardAdapter(
+            applicationContext,
+            Layout.VERTICAL
+        )
+
+        // Specify fixed size to improve performance
+        binding.verticalRecyclerView.setHasFixedSize(true)
+
+        // Enable up button for backward navigation
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
